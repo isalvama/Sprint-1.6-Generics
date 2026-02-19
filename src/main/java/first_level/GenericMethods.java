29ec74760ace1ea27085097f66c9a6c4b6fcc0ac
isalvama <inesalvans@gmail.com>
2026-02-19 13:58:12 +0100

    feat: creation of GenericMethods class to test the performance of generic methods

---
+++ src/main/java/first_level/GenericMethods.java
@@ -0,0 +1,18 @@
+package first_level;
+
+public class GenericMethods  <T, U, V> {
+    private T element1;
+    private U element2;
+    private V element3;
+
+    public GenericMethods(T element1, U element2, V element3){
+        this.element1 = element1;
+        this.element2 = element2;
+        this.element3 = element3;
+    }
+
+    public void printElements(){
+        System.out.println("element1 = " + element1 + "\n element2 = " + element2 + "\n element3 = " + element3);
+    }
+
+}
