deb752d606c653f65a5a6d2ad6a4b05274b8fa47
isalvama <inesalvans@gmail.com>
2026-02-19 14:02:12 +0100

    feat: creation of SecondLevelGEnericMethods with a generic constructor 'SecondLevelGenericMethods()' and a varargs generic method 'printAllElements()'

---
+++ src/main/java/second_level/SecondLevelGenericMethods.java
@@ -0,0 +1,26 @@
+package second_level;
+
+public class SecondLevelGenericMethods<String, T, U, V> {
+    private String stringVariable;
+    private T element1;
+    private U element2;
+    private V element3;
+
+    public SecondLevelGenericMethods(String stringVariable, T element1, U element2, V element3){
+        this.stringVariable = stringVariable;
+        this.element1 = element1;
+        this.element2 = element2;
+        this.element3 = element3;
+    }
+
+    public void printElements(){
+        System.out.println("stringVariable = " + stringVariable + "element1 = " + element1 + "\n element2 = " + element2 + "\n element3 = " + element3);
+    }
+
+    public void printAllElements(String message, T ... objects){
+        System.out.println(message);
+        for (T element : objects){
+            System.out.println(element);
+        }
+    }
+}
