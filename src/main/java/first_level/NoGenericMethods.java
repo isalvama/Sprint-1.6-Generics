f0eca0a634139b2ce8bbd9146ea596c1c9e8412c
isalvama <inesalvans@gmail.com>
2026-02-19 13:57:53 +0100

    feat: creation of NoGenericMethods class to test the performance of non-generic methods

---
+++ src/main/java/first_level/NoGenericMethods.java
@@ -0,0 +1,25 @@
+package first_level;
+
+public class NoGenericMethods {
+    private final int element1;
+    private final int element2;
+    private final int element3;
+
+    public NoGenericMethods(int element1, int element2, int element3){
+        this.element1 = element1;
+        this.element2 = element2;
+        this.element3 = element3;
+    }
+
+    public int getElement1() {
+        return element1;
+    }
+
+    public int getElement2() {
+        return element2;
+    }
+
+    public int getElement3() {
+        return element3;
+    }
+}
