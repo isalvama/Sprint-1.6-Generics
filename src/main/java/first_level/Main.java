c627132caafa60fe63fea49b1994929c71e63935
isalvama <inesalvans@gmail.com>
2026-03-16 20:53:55 +0100

    feat: add spaces between lines to enhance format and readability

--- src/main/java/first_level/Main.java
+++ src/main/java/first_level/Main.java
@@ -5,20 +5,26 @@ public class Main {
 
         NoGenericMethods noGenericMethod1 = new NoGenericMethods(1, 2, 3);
         NoGenericMethods noGenericMethod2 = new NoGenericMethods(3, 1, 2);
+
         System.out.println(noGenericMethod1.getElement1() + noGenericMethod1.getElement2() + noGenericMethod1.getElement3());
         System.out.println(noGenericMethod2.getElement1() + noGenericMethod1.getElement2() + noGenericMethod1.getElement3());
+
         Person person1 = new Person("Joe", "Wright", 30);
         Person person2 = new Person("Laura", "Alvarez", 60);
-        GenericMethods<String, Person, Integer> gm1 = new GenericMethods<>("Hello", person1, 10);
-        System.out.println("gm1 elements:");
-        gm1.printElements();
-        GenericMethods<String, String, Person> gm2 = new GenericMethods<>("Hello", "World", person2);
-        System.out.println("gm2 elements:");
-        gm2.printElements();
+
+        GenericMethods<String, Person, Integer> genericObject1 = new GenericMethods<>("Hello", person1, 10);
+        System.out.println("genericMethod1 elements:");
+        genericObject1.printElements();
+
+        GenericMethods<String, String, Person> genericObject2 = new GenericMethods<>("Hello", "World", person2);
+        System.out.println("genericMethods elements:");
+        genericObject2.printElements();
+
         GenericMethods<Integer, String, Person> gm3 = new GenericMethods<>(35, "World", person1);
         System.out.println("gm3 elements:");
         gm3.printElements();
-        GenericMethods<GenericMethods, Person, Person> gm4 = new GenericMethods<>(gm2, person1, person2);
+
+        GenericMethods<GenericMethods, Person, Person> gm4 = new GenericMethods<>(genericObject2, person1, person2);
         System.out.println("gm4 elements:");
         gm4.printElements();
     }
